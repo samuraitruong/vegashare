93eb69c901d5cabbc0eb305c3878e24f264ef152
Truong Nguyen <samuraitruong@hotmail.com>
2025-09-11 18:02:04 +1000

    Remove obsolete files and update auto-sync workflow
    - Deleted multiple outdated tournament-related files, including PGN, HTML, and JSON files across various directories.
    - Updated the auto-sync workflow configuration to reflect the removal of these files and ensure proper functioning.
    - Cleaned up unnecessary assets to streamline the project structure.

--- vega/www2025HobsonsBayJuniorsRapidChampionship/pairing.php
+++
@@ -1,4 +0,0 @@
-<nav class="me-0 me-lg-3" aria-label="Page navigation">
-          <ul class="pagination mb-lg-0">
-     </ul>
-  </nav>
--- www/www2025HobsonsBayJuniorsRapidChampionship/game.html
+++
@@ -1,1 +0,0 @@
- <h4>Download PGN:</h4> <div class="btn-toolbar"> <div class="btn-group"></div> </div>
